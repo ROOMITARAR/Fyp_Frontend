81ec20e42048dcecc21ac7f794bc385ffdca8e25
ROOMITARAR <roomi6067@gmail.com>
2026-05-10 14:43:10 +0500

    build: update production API endpoint to deployed Vercel backend URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:3001' // update this to your production API URL
+  apiUrl: 'https://auth-backend-e10c8jml9-abdulrehmanrumitarar-3464s-projects.vercel.app'
 };
